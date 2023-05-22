ccd8bc28f7f6a8f4946ce1363c18250ab02b97e2
Marina <95050756+Marindala@users.noreply.github.com>
2023-05-22 16:55:52 -0300

    Route /register para componet fomulario Doc

--- qali-client/src/App.jsx
+++ qali-client/src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route } from "react-router-dom";
 //Components
 import Home from "./components/Home/Home";
+import { FormDoctor } from "./components/FormDoctor/FormDoctor";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/register" element={<FormDoctor/>}/>
       </Routes>
     </>
   )
