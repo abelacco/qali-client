63a64e136e3214f3908881ad6e7785fbada0078e
SantiagoOscarVera <santiagovera280@gmail.com>
2023-05-25 00:36:39 -0300

    form pacientes

--- qali-client/src/App.jsx
+++ qali-client/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Especialistas from "./components/Especialistas/Especialistas";
 import  FormDoctor  from "./components/FormDoctor/FormDoctor.jsx";
+import FormPatient from "./components/FormPatient/FormPatient";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/Especialistas" element={<Especialistas/>}/>
         <Route exact path="/register" element={<FormDoctor/>}/>
+        <Route exact path="/register/patient" element={<FormPatient/>}/>
       </Routes>
     </>
   )
