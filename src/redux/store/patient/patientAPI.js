d88091e5d85ee59fb69750ab8e6ac815f3d4f338
Leandro <leandrodevlarrea@hotmail.com>
2023-06-17 18:55:25 -0300

    url deploy

--- src/redux/store/patient/patientAPI.js
+++ src/redux/store/patient/patientAPI.js
@@ -2,15 +2,11 @@ import axios from "axios";
 
 export const createPatient = (patient) => {
   return axios.post(
-    "http://localhost:3000/patient" ||
-      "https://qali-api-production.up.railway.app/patient",
+    "https://qali-api-production.up.railway.app/patient",
     patient
   );
 };
 
 export const getPatient = () => {
-  return axios.get(
-    "http://localhost:3000/patient" ||
-      "https://qali-api-production.up.railway.app/patient"
-  );
+  return axios.get("https://qali-api-production.up.railway.app/patient");
 };
